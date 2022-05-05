c3652c12c606c7442059abafe882e239615cb47a
Myzwer <josh@onefortyfivedesign.com>
2022-05-05 15:04:53 -0400

    :rocket: Updated app to toggle between dark and light mode based on button click. Background also switches color, and the darkMode const is exported via props so card.vue can access it.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <script setup>
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import {ref} from "vue";
 import card from './components/card.vue'
+
+const darkMode = ref(false);
+  props: ['darkMode'];
+
 
 </script>
 
 <template>
-  <h1 class="text-center text-4xl uppercase">Foothills church</h1>
-  <h2 class="text-center text-2xl uppercase pb-10">Request Within</h2>
-  <div class="max-w-6xl mx-auto">
-    <hr>
-    <div class="grid grid-cols-12 gap-4 ">
-      <card />
+  <div :class="{ 'bg-black': darkMode }">
+    <h1 class="pt-20 text-center text-4xl uppercase">Foothills church</h1>
+    <h2 class="text-center text-2xl uppercase pb-10">Request Within</h2>
+    <div class="max-w-6xl mx-auto">
+      <hr>
+      <div class="grid grid-cols-12 gap-4 ">
+        <card/>
+      </div>
+    </div>
+
+    <div class="mx-auto text-center py-12">
+      <button
+          @click="darkMode = !darkMode"
+          class="mb-20 uppercase md:inline-block rounded-md py-3 px-6 transition duration-300"
+          :class="{ 'bg-gray-dark hover:bg-gray-darkest text-white': !darkMode, 'bg-gray-light hover:bg-gray-lightest text-black': darkMode }">
+        <span v-if="darkMode">Enable Light Mode</span>
+        <span v-else>Enable Dark Mode</span>
+      </button>
     </div>
   </div>
-
-
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
